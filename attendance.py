283975ed0a302ac0510bbda42dd007040de1d212
Abe Handler <abram.handler@gmail.com>
2021-01-27 10:11:20 -0700

    just backup

--- attendance.py
+++ attendance.py
@@ -5,27 +5,23 @@ attendanceCount = defaultdict(int)
 
 DIR = "3402"
 for fileName in os.listdir(DIR):
-    if (fileName != ".ipynb_checkpoints" and "processed_" not in fileName):
-        print(fileName)
+    if (fileName != ".ipynb_checkpoints" and "processed" not in fileName):
+        print("[*] processing " + fileName)
         with open(os.path.join(DIR, fileName), 'r') as currentFile:
             if (fileName.split("_")[0] == "participants"):
-                # attendance file
-                # skip header
                 csvReader = csv.reader(currentFile)
-                #print
-                #next(csvReader)
+                next(csvReader)
                 for row in csvReader:
                     minutes = row[-2] # total minutes. Canvas makes 2 rows 
                     if (row[1].find("@") != -1): # has an email
                         attendanceCount[row[1]] += int(minutes)
 
+        fn = DIR + "/processed_" + fileName
 
-    fn = DIR + "/processed_" + fileName
-
-    attendanceFile = open(fn, "w")
-    for key, value in sorted(attendanceCount.items()):
-        #print(key+": "+str(value))
-        # write to file
-        if value >= 40: # must attend 40 minues
-            attendanceFile.write(key+","+str(value)+"\n")
-    attendanceFile.close()
+        attendanceFile = open(fn, "w")
+        for key, value in sorted(attendanceCount.items()):
+            #print(key+": "+str(value))
+            # write to file
+            if value >= 40: # must attend 40 minues
+                attendanceFile.write(key+","+str(value)+"\n")
+        attendanceFile.close()
