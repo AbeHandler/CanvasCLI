999d907331d042569f3a4015abe6b2e5789c9ac5
Abe Handler <abram.handler@gmail.com>
2021-01-27 11:58:59 -0700

    just backup

--- attendance.py
+++ attendance.py
@@ -1,16 +1,22 @@
 from collections import defaultdict
 import os
 import csv
-attendanceCount = defaultdict(int)
+
 
 DIR = "3402"
 for fileName in os.listdir(DIR):
     if (fileName != ".ipynb_checkpoints" and "processed" not in fileName):
         print("[*] processing " + fileName)
+
+        date = fileName.split("_")[-1].replace(".csv", "")
+        
+        attendanceCount = defaultdict(int)
+
         with open(os.path.join(DIR, fileName), 'r') as currentFile:
             if (fileName.split("_")[0] == "participants"):
                 csvReader = csv.reader(currentFile)
                 next(csvReader)
+
                 for row in csvReader:
                     minutes = row[-2] # total minutes. Canvas makes 2 rows 
                     if (row[1].find("@") != -1): # has an email
@@ -18,10 +24,10 @@ for fileName in os.listdir(DIR):
 
         fn = DIR + "/processed_" + fileName
 
-        attendanceFile = open(fn, "w")
-        for key, value in sorted(attendanceCount.items()):
-            #print(key+": "+str(value))
-            # write to file
-            if value >= 40: # must attend 40 minues
-                attendanceFile.write(key+","+str(value)+"\n")
-        attendanceFile.close()
+        with open(fn, "w") as of:
+            of.write("email,present,date\n")
+            for key, value in sorted(attendanceCount.items()):
+                #print(key+": "+str(value))
+                # write to file
+                present = 1 if (value >= 40) else 0
+                of.write(key + ", " + str(present) + "," + date + "\n")
