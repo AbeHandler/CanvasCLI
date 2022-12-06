2f9f81433f5a21f9b9ea45f0ff98bdfee7821a0f
Abe Handler <akh2103@gmail.com>
2022-12-06 05:06:48 -0700

    grader

--- src/grader.py
+++ src/grader.py
@@ -6,37 +6,53 @@ from pathlib import Path
 
 class Grader(object):
 
-	def __init__(self, course: Course):
-		self.course = course 
+    def __init__(self, course: Course):
+        self.course = course 
 
 
-	def download_all_submitted_files_with_extension(self,
-													assignment_id: str = "1533042",
-	                                                extension: str = ".py",
-	                                                download_base_directory: Path = Path("/tmp")
-	                                                ):
-	    '''
-	    Downloads all files with a given extension to download_base_directory/assignment_id
-	    '''
-	    assignment = course.get_assignment(assignment_id)
-	    base_directory: Path = download_base_directory / assignment_id
-	    
-	    for submission in assignment.get_submissions():
-	        for attachment in submission.attachments:
-	            file_name = attachment.filename
-	            if file_name.endswith(extension):
-	                
-	                save_subdir = base_directory / str(submission.user_id)
-	                save_subdir.mkdir(parents=True, exist_ok=True)
-	                
-	                save_here = save_subdir / file_name 
-	                attachment.download(save_here.as_posix())
-	                print(f"[*] Downloaded {save_here.as_posix()}")
+    def download_all_submissions_for_assigment(self,
+                                               assignment_id: str = "1533042",
+                                               include_extension: str = ".py",
+                                               download_base_directory: Path = Path("/tmp")
+                                               ):
+        '''
+        Downloads all files with a given extension to download_base_directory/assignment_id
+        '''
+        assignment = course.get_assignment(assignment_id)
+
+        local_download_location: Path = download_base_directory / assignment_id
         
+        for submission in assignment.get_submissions():
+            for attachment in submission.attachments:
+                if attachment.filename.endswith(include_extension):
+                    
+                    save_here = self._get_local_path(download_base_directory = download_base_directory,
+                                                     assignment_id = str(assignment_id),
+                                                     user_id = str(submission.user_id),
+                                                     file_name = str(attachment.filename))
+                    attachment.download(save_here.as_posix())
+                    print(f"[*] Downloaded {save_here.as_posix()}")
+        
+    def _get_local_directory(self,
+                             download_base_directory: Path,
+                             assignment_id: str,
+                             user_id: str) -> Path:
+        save_here = download_base_directory / assignment_id / user_id
+        save_here.mkdir(parents=True, exist_ok=True)
+        return save_here
+
+    def _get_local_path(self,
+                        download_base_directory: Path,
+                        assignment_id: str,
+                        user_id: str,
+                        file_name: str) -> Path:
+        
+        local_dir = self._get_local_directory(download_base_directory, assignment_id, user_id)
+        return local_dir / file_name
 
 if __name__ == "__main__":
-	api = get_api()
-	config = Config(Path("/Users/abe/CanvasCLI/3220S2023.ini"))
-	course = api.get_course(config.canvas_no)
-	grader = Grader(course)
-	grader.download_all_submitted_files_with_extension(extension="png")
+    api = get_api()
+    config = Config(Path("/Users/abe/CanvasCLI/3220S2023.ini"))
+    course = api.get_course(config.canvas_no)
+    grader = Grader(course)
+    grader.download_all_submissions_for_assigment(include_extension="png")
