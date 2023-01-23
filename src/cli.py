403a7e16090ae61cb0934026f98e2b94e3522711
Abe Handler <akh2103@gmail.com>
2023-01-22 20:38:50 -0700

    ok

--- src/cli.py
+++ src/cli.py
@@ -29,8 +29,6 @@ and aliasing it as "canvas"
 
 """
 
-
-
 import argparse
 import glob
 import os
@@ -117,70 +115,6 @@ def create_in_class_assignment(
 
 
 
-def get_ungraded_in_class_assignments_for_course(course):
-    """
-    Return the in-class assignments for a course if no grades have
-    been assigned yet for the whole class
-    """
-    ids = set()
-    for j in get_in_class_assignments_for_course(course):
-        any_graded = False
-        for user in course.get_users(enrollment_type=["student"]):
-            sub = j.get_submission(user=user.id)
-            if sub.graded_at != None:
-                any_graded = True
-        if not any_graded:
-            ids.add(j.id)
-    return ids
-
-
-def grade_in_class_assignments(course):
-    """Grade ungraded in-class assignments (for participation)"""
-    for assignment_id in get_ungraded_in_class_assignments_for_course(course):
-        assignment = course.get_assignment(assignment_id)
-        gradable_students = assignment.get_gradeable_students()
-        if type(gradable_students) == PaginatedList:
-            grade = True
-        else:
-            if len(gradable_students) > 0:
-                grade = True
-
-        if grade is True:
-            print("\t Scoring {}".format(assignment.name))
-            for student in gradable_students:
-                comment_and_grade_participation(
-                    assignment.id, student, course=course
-                )
-
-
-def get_names2ids(CUnum2canvasnum):
-    names2ids = {}
-    for coursename, courseno in CUnum2canvasnum.items():
-        names2ids[coursename] = get_student_names2_ids(
-            CUnum2canvasnum[coursename]
-        )
-    return names2ids
-
-
-
-def get_due_from_args(args) -> str:
-    if args.due is None:
-
-        if args.tomorrow is False:
-            print("[*] No due date, assuming it's for today")
-            return date.today().strftime("%Y%m%d")
-        else:
-            print("[*] Assignment is for tomorrow")
-            return (date.today() + timedelta(days=1)).strftime("%Y%m%d")
-    elif args.due is not None:
-        try:
-            if args.due is not None:
-                return datetime.strptime(args.due, "%Y%m%d").strftime("%Y%m%d")
-        except ValueError:
-            print(
-                "[*] The argument inClass needs to match the format YYYYMMDD. Won't make assignment."
-            )
-
 def get_course_from_ini(path_to_ini: str = "/Users/abe/CanvasCLI/3220S2023.ini"):
     '''
     A convenience function which creates a course object
@@ -225,49 +159,3 @@ if __name__ == "__main__":
 
     if args.export:
         course.export()
-
-    # the rest is old code and suspect
-
-    import os; os._exit(0)
-
-    if args.zeros and args.assignment_id is not None:
-        # py canvas_cli.py -c 2301 -zeros --assignmentid 871212
-        course = canvas.get_course(CUnum2canvasnum[args.course])
-        for student in get_no_submissions(course, args.assignment_id):
-            comment_and_grade_no_submission(args.assignment_id, student)
-        os._exit(0)
-
-    if args.set_extra_time_on_quizzes:
-        names2ids = get_names2ids(CUnum2canvasnum)
-        course = canvas.get_course(CUnum2canvasnum[args.course])
-        names = [
-            o.replace("\n", "")
-            for o in open("accomodations{}.txt".format(args.course))
-        ]
-        names2ids_course = names2ids[args.course]
-        set_extra_time_on_quizzes(course, names, names2ids_course)
-        os._exit(0)
-
-    if args.assignment:
-
-        course = canvas.get_course(CUnum2canvasnum[args.course])
-
-        due = get_due_from_args(args)
-        assert type(due) == str
-
-        assignment = create_in_class_assignment(
-            courseNo=args.course,
-            due=due,
-            name=args.name,
-            published=True,
-            points=args.points,
-        )
-
-        canvas_no = CUnum2canvasnum[args.course]
-        main_page = Canvasno2mainpage[canvas_no]
-        link_url_for_in_class_assignment(
-            assignment=assignment, main_page=main_page, due=due, course=course
-        )
-
-        os._exit(0)
-
