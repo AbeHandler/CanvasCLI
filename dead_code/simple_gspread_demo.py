a4c074aa51f88faa07e9fb97d3b72ca32e385fd8
Abe Handler <akh2103@gmail.com>
2023-01-27 05:37:02 -0700

    refactor: remove a folder holding old code

--- dead_code/simple_gspread_demo.py
+++
@@ -1,9 +0,0 @@
-import gspread
-
-### https://gspread.readthedocs.io/en/latest/oauth2.html#oauth-client-id
-gc = gspread.oauth()
-
-# This needs to exist already 
-sh = gc.open("Quiz correction (Responses)")
-
-print(sh)
