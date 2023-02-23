f61b3238374dcb46d0b1c85b20d9db7779771e19
Abe Handler <akh2103@gmail.com>
2023-02-23 08:00:59 -0700

    starting nbgrader parser

--- src/auto_graded_notebook.py
+++ src/auto_graded_notebook.py
@@ -1,4 +1,5 @@
 from typing import Dict, List
+import json
 
 class NotebookCell:
     def __init__(self, cell_type: str, execution_count: int, id: str, metadata: Dict, outputs: List, source: List[str]):
@@ -9,6 +10,7 @@ class NotebookCell:
         self.outputs = outputs
         self.source = source
 
+'''
 class Nbgrader:
     def __init__(self, cell_type: str, checksum: str, grade: bool, grade_id: str, locked: bool, points: int, schema_version: int, solution: bool, task: bool):
         self.cell_type = cell_type
@@ -36,3 +38,39 @@ class Notebook:
         self.metadata = metadata
         self.nbformat = nbformat
         self.nbformat_minor = nbformat_minor
+'''
+
+class NotebookParser:
+    def __init__(self, filepath):
+        self.filepath = filepath
+
+    def parse(self) -> List[NotebookCell]:
+        out = []
+        with open(self.filepath, "r") as inf:
+            dt = json.load(inf)
+            for cell in dt["cells"]:
+                cell = self.create_notebook_cell(cell)
+                out.append(cell)
+        return out
+    
+    def create_notebook_cell(self, cell):
+        cell_type = cell["cell_type"]
+        try:
+            execution_count = cell["execution_count"]
+        except:
+            execution_count = 0
+        _id = cell["id"]
+        metadata = cell["metadata"]
+        try:
+            outputs = cell["outputs"]
+        except:
+            outputs = []
+        source = cell["source"]
+        
+        return NotebookCell(cell_type, execution_count, _id, metadata, outputs, source)
+
+if __name__ == "__main__":
+
+    parser = NotebookParser("test/fixtures/four.ipynb")
+    cells = parser.parse()
+    cells[0].metadata
