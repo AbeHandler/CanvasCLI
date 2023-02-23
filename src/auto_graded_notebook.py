deda012c957808803c378cc180b3e812a0c8ce6e
Abe Handler <akh2103@gmail.com>
2023-02-23 09:49:01 -0700

    add check for not implemented

--- src/auto_graded_notebook.py
+++ src/auto_graded_notebook.py
@@ -2,43 +2,16 @@ from typing import Dict, List
 import json
 
 class NotebookCell:
-    def __init__(self, cell_type: str, execution_count: int, id: str, metadata: Dict, outputs: List, source: List[str]):
+    def __init__(self, cell_type: str, execution_count: int, id: str, metadata: Dict, outputs: List, source: List[str], points: int, not_implemented: bool):
         self.cell_type = cell_type
         self.execution_count = execution_count
         self.id = id
         self.metadata = metadata
         self.outputs = outputs
         self.source = source
-
-'''
-class Nbgrader:
-    def __init__(self, cell_type: str, checksum: str, grade: bool, grade_id: str, locked: bool, points: int, schema_version: int, solution: bool, task: bool):
-        self.cell_type = cell_type
-        self.checksum = checksum
-        self.grade = grade
-        self.grade_id = grade_id
-        self.locked = locked
         self.points = points
-        self.schema_version = schema_version
-        self.solution = solution
-        self.task = task
+        self.not_implemented = not_implemented
 
-class NotebookMetadata:
-    def __init__(self, deletable: bool, editable: bool, execution: Dict, id: str, nbgrader: Nbgrader, tags: List[str]):
-        self.deletable = deletable
-        self.editable = editable
-        self.execution = execution
-        self.id = id
-        self.nbgrader = nbgrader
-        self.tags = tags
-
-class Notebook:
-    def __init__(self, cells: List[NotebookCell], metadata: NotebookMetadata, nbformat: int, nbformat_minor: int):
-        self.cells = cells
-        self.metadata = metadata
-        self.nbformat = nbformat
-        self.nbformat_minor = nbformat_minor
-'''
 
 class NotebookParser:
     def __init__(self, filepath):
@@ -67,10 +40,23 @@ class NotebookParser:
             outputs = []
         source = cell["source"]
         
-        return NotebookCell(cell_type, execution_count, _id, metadata, outputs, source)
+        points = 0
+        # not all cells are graded
+        if "nbgrader" in metadata:
+            if "points" in metadata["nbgrader"]:
+                points = int(metadata["nbgrader"]["points"])
+
+        not_implemented = False
+        for _ in outputs:
+            if "ename" in _:
+                if _['ename'] == 'NotImplementedError':
+                    not_implemented = True
+
+        return NotebookCell(cell_type, execution_count, _id, metadata, outputs, source, points, not_implemented)
 
 if __name__ == "__main__":
 
     parser = NotebookParser("test/fixtures/four.ipynb")
     cells = parser.parse()
-    cells[0].metadata
+    for cell in cells:
+        print(cell.not_implemented)
