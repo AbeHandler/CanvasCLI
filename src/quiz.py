850d45ad4cc06e7249ba1fcdefeedd6cfb46afe4
Abe Handler <akh2103@gmail.com>
2023-08-28 09:12:37 -0600

    update

--- src/quiz.py
+++ src/quiz.py
@@ -1,4 +1,8 @@
 import random
+from pathlib import Path
+from src.config import Config
+from src.api import get_api
+from src.course import Course
 
 class Quiz(object):
 
@@ -17,16 +21,31 @@ class Quiz(object):
 
 
     def create_question(self, question_name: str = "Lorem impsum",
-                        question_text: str = 'What is Lorem impsum?'):
+                        question_text: str = 'What is Lorem impsum?',
+                        points_possible: int = 4):
         id_ = random.randint(0, 10000) # TODO how to assign id_ in a pricipled way
-        self._quiz.create_question(**{"id": id_, 
-                                      "quiz_id": self.quiz_id, 
-                                      "question_name": question_name, 
-                                      "question_text": question_text})
+        print(question_name, question_text)
+
+        question_data = {
+            'question': {
+                'question_name': question_name,
+                'question_text': question_text,
+                'points_possible': points_possible,
+                "question_type": "numerical_question",
+                #'answers': [
+                #    {'answer_text': 'Choice 1', 'weight': 100}
+                #]
+            }
+        }
+
+        print(question_data)
+        self._quiz.create_question(**question_data)
 
 if __name__ == "__main__":
-    path = Path("/Users/abe/CanvasCLI/3220S2023.ini")
+    path = Path("/Users/abe/CanvasCLI/3220F2023.ini")
     config = Config(path)
     api = get_api()
     course = Course(config=config, api=api)
-    quiz = Quiz(course, 308165)
+    quiz = Quiz(course, 337161)
+    q = quiz.create_question(question_name="Participation",
+                             question_text="tess2ts")
